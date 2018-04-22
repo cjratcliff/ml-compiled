498a767cc80b5142dfb591170845955580450437
Chris Ratcliff <c.j.ratcliff@gmail.com>
2018-04-22 22:16:54 +0100

    Create cookie_consent.js

---
+++ _static/cookie_consent.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#000"
+    },
+    "button": {
+      "background": "#f1d600"
+    }
+  }
+})});

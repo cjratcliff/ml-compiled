ef2c0bffde5c1867947467938b8fe20b63953def
Chris Ratcliff <c.j.ratcliff@gmail.com>
2018-04-24 00:57:45 +0100

    Create cookies.js

---
+++ js/cookies.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#252e39"
+    },
+    "button": {
+      "background": "#14a7d0"
+    }
+  },
+  "theme": "classic"
+})});

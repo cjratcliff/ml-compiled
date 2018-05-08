a64ed2aab25b644651d9ab07564b8be872ea53ba
Chris Ratcliff <c.j.ratcliff@gmail.com>
2018-05-08 23:30:00 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+   name='ml-compiled',
+   version='0.1',
+   description='Quick definitions and intuitive explanations around machine learning',
+   author='Chris Ratcliff',
+   author_email='c.j.ratcliff@gmail.com',
+   packages=['ml-compiled'],  #same as name
+    install_requires=[
+        'fs (~= 2.0)', 'graphviz (>= 0.8, < 0.9)', 'jinja2 (~= 2.9)', 'mondrian (~= 0.6)', 'packaging (~= 16.0)',
+        'psutil (~= 5.4)', 'python-slugify (~= 1.2.0)', 'requests (~= 2.0)', 'stevedore (~= 1.27)', 'whistle (~= 1.0)'
+    ],
+    extras_require={
+        'dev': [
+            'cookiecutter (>= 1.5, < 1.6)', 'coverage (>= 4.4, < 5.0)', 'pytest (>= 3.1, < 4.0)',
+            'pytest-cov (>= 2.5, < 3.0)', 'pytest-sugar (>= 0.8, < 0.9)', 'pytest-timeout (>= 1, < 2)',
+            'sphinx (>= 1.6, < 2.0)', 'sphinx-sitemap (>= 0.2, < 0.3)', 'yapf'
+        ],
+    },
+)
